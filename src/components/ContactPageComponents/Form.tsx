61be30549479c43ee74c9375c4459bb9ba27fbb5
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-14 14:41:00 +0300

    lottie

--- src/components/ContactPageComponents/Form.tsx
+++ src/components/ContactPageComponents/Form.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
-
+import Lottie from "lottie-react";
+import lottieAnime from "../../assets/lottieAnime.json"
 const Form = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: lottieAnime,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+
     const handleSubmit=(e: React.FormEvent)=>{
         e.preventDefault()
     }
@@ -9,22 +20,25 @@ const Form = () => {
 	<div className="flex flex-col justify-between">
 		<div className="space-y-2">
 			<h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
-			<div className="dark:text-gray-400">Vivamus in nisl metus? Phasellus.</div>
+			<div className="dark:text-gray-400">Get in touch with us...</div>
 		</div>
-		<img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
+		{/* <img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" /> */}
+        <div >
+        <Lottie animationData={lottieAnime} loop={true} />
+        </div>
 	</div>
 	<form  className="space-y-6 ng-untouched ng-pristine ng-valid" onSubmit={handleSubmit}>
 		<div>
 			<label htmlFor="name" className="text-sm">Full name</label>
-			<input id="name" type="text" placeholder="" className="w-full p-3 border-2 rounded-xl dark:bg-gray-800" />
+			<input id="name" type="text" placeholder="Your Name*" className="w-full p-3 border-2 rounded-xl dark:bg-gray-800" />
 		</div>
 		<div>
 			<label htmlFor="email" className="text-sm">Email</label>
-			<input id="email" type="email" className="w-full p-3 rounded-xl border-2 dark:bg-gray-800" />
+			<input id="email" type="email" className="w-full p-3 rounded-xl border-2 dark:bg-gray-800" placeholder='Email' />
 		</div>
 		<div>
 			<label htmlFor="message" className="text-sm">Message</label>
-			<textarea id="message" rows={3} className="w-full p-3 rounded-xl border-2 dark:bg-gray-800"></textarea>
+			<textarea id="message" rows={3} placeholder='message*' className="w-full p-3 rounded-xl border-2 dark:bg-gray-800"></textarea>
 		</div>
 		<button
                style={{
