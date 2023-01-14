530dd6b8a94900b5ab7318b6f66d13c6ed6ae71a
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-14 14:06:11 +0300

    add Contact page

---
+++ src/components/ContactPageComponents/Map.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div>
+    <div className="mapouter rounded-lg">
+      <div className="gmap_canvas">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127357.71188581262!2d39.596222583956944!3d-4.035014502468972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x184012e78ec02c7d%3A0xcb618bbc35d0db5a!2sMombasa!5e0!3m2!1sen!2ske!4v1668111231878!5m2!1sen!2ske"
+        className="border-0 w-full h-[450px]"
+        allowFullScreen={false}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      </div>
+</div>
+  </div>
+  )
+}
+
+export default Map
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import AboutHeader from '../components/AboutPageComponents/AboutHeader'
+import Map from '../components/ContactPageComponents/Map'
+import Form from '../components/ContactPageComponents/Form'
 
 const Contact = () => {
   return (
     <>
     <AboutHeader/>
+    <div className=''>
+    <Map />
+    <Form/>
+    </div>
     </>
   )
 }
